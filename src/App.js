1ea07c73a94bcc18b42bcef34cc74b41c8181124
24ERIC <60751655+24ERIC@users.noreply.github.com>
2022-10-16 01:22:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -52,21 +52,7 @@ function App() {
 						<h1 class="text-big" id="program">
 							JavaScript Tutorial
 						</h1>
-						<p class="text-small">
-							JavaScript is the world most popular
-							lightweight, interpreted compiled
-							programming language. It is also
-							known as scripting language for
-							web pages. It is well-known for
-							the development of web page many
-							non-browser environments also use
-							it. JavaScript can be used for
-							Client-side developments as well
-							as Server-side developments.
-						</p>
-					</div>
-				</div>
-			</section>
+						<
 			<section class="section">
 				<div class="box-main">
 					<div class="secondHalf">
