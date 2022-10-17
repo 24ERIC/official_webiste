ba6dd579daf5f5d68f183b6ac0e2031548be3cc4
24ERIC <60751655+24ERIC@users.noreply.github.com>
2022-10-17 18:57:55 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Webpages from './Top_Folder/1_utm021_webiste_webpage/index.jsx';
+import reportWebVitals from './reportWebVitals';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-import ReactDOM from 'react-dom/client';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Contact from './Top_Folder/1_utm021_webiste_webpage/Contact';
-// import Home from './Top_Folder/1_utm021_webiste_webpage/Home';
-// import NoPage from './Top_Folder/1_utm021_webiste_webpage/NoPage';
-// import Projects from './Top_Folder/1_utm021_webiste_webpage/Content_Upload';
-
-// export default function Appp() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route index element={<Home />} />
-//         <Route path="projects" element={<Projects />} />
-//         <Route path="contact" element={<Contact />} />
-//         <Route path="*" element={<NoPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
