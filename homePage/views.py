f1803e8a57f509eb5b5aaa46eeedbb85b2834c11
24ERIC <60751655+24ERIC@users.noreply.github.com>
2022-12-14 07:41:41 -0500

    Test website with pqges

--- homePage/views.py
+++ homePage/views.py
@@ -13,5 +13,32 @@ def index(request):
             <p>The current time is { now }.</p>
         </body>
     </html>
+
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            <form method="POST">
+                
+                <input type="submit" value="Submit">
+            </form>
+
+        </body>
+
+    </html>
+
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
     '''
-    return HttpResponse(html)
+    # return HttpResponse(html)
+    return render(request, 'home.html', {"a": "rgv"})
