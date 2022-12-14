b0b62fd09afdbe90808f625198b8a4260eb725f7
24ERIC <60751655+24ERIC@users.noreply.github.com>
2022-12-14 10:31:57 -0500

    Finished the website basic structure

--- homePage/views.py
+++ homePage/views.py
@@ -5,40 +5,4 @@ from datetime import datetime
 from django.http import HttpResponse
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-
-    <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-
-        <body>
-            <form method="POST">
-                
-                <input type="submit" value="Submit">
-            </form>
-
-        </body>
-
-    </html>
-
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    # return HttpResponse(html)
-    return render(request, 'home.html', {"a": "rgv"})
+    return render(request, 'home.html')
