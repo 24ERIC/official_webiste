3951c362a2c1b55805109cee7c6ee5882d1aba2d
24ERIC <60751655+24ERIC@users.noreply.github.com>
2022-12-16 14:21:31 -0500

    I can use js directly in html nice, good job

--- home/views.py
+++ home/views.py
@@ -4,6 +4,19 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 
+from json import dumps
+
 def index(request):
-    
-    return render(request, 'home.html')
+    # create data dictionary
+    dataDictionary = {
+    'hello': 'World',
+    'geeks': 'forgeeks',
+    'ABC': 123,
+    456: 'abc',
+    14000605: 1,
+    'list': ['geeks', 4, 'geeks'],
+    'dictionary': {'you': 'can', 'send': 'anything', 3: 1}
+    }
+    # dump data
+    dataJSON = dumps(dataDictionary)
+    return render(request, 'home.html', {'data': dataJSON})
